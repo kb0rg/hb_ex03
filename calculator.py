647fce2f6e074913d77db82f230b05c1acc583b7
Hackbright Student <info@hackbrightacademy.com>
2015-01-15 12:57:12 -0800

    Fixed bad input, however, broke all other operations

--- calculator.py
+++ calculator.py
@@ -18,7 +18,7 @@ print """Welcome to our prefix calculator!
 Please enter an operation first, then the number
 or numbers you would like to calculate.
 enter an operator from the following:
-+ - * / % square cube power mod
++ - * / square cube power mod
 Enter 'q' to quit calculator
 """
 
@@ -29,34 +29,40 @@ while True:
 
     if tokens[0] == "q":
         break
+    
+    if tokens[0] not in ["cube", "square", "power", "mod"]:
+        if tokens[0].isalpha() or tokens[1].isalpha() or tokens[2].isalpha():
+            print "Sorry, that is not a valid entry. \n Please enter a valid operator followed by a number or numbers"
 
-    num1 = float(tokens[1])
-    if tokens[0] != "cube" and tokens[0] != "square" and tokens[0] != "q": 
-        num2 = float(tokens[2])
+    else:
+        
+        num1 = float(tokens[1])
+        if tokens[0] != "cube" and tokens[0] != "square" and tokens[0] != "q": 
+            num2 = float(tokens[2])
 
-    if tokens[0] == "+":
-        print arithmetic.add(num1, num2)
+        if tokens[0] == "+":
+            print arithmetic.add(num1, num2)
 
-    if tokens[0] == "-":
-        print arithmetic.subtract(num1, num2)
+        if tokens[0] == "-":
+            print arithmetic.subtract(num1, num2)
 
-    if tokens[0] == "*":
-        print arithmetic.multiply(num1, num2)
+        if tokens[0] == "*":
+            print arithmetic.multiply(num1, num2)
 
-    if tokens[0] == "/":
-        print float(arithmetic.divide(num1, num2))
+        if tokens[0] == "/":
+            print float(arithmetic.divide(num1, num2))
 
-    if tokens[0] == "square":
-        print arithmetic.square(num1)
+        if tokens[0] == "square":
+            print arithmetic.square(num1)
 
-    if tokens[0] == "cube":
-        print arithmetic.cube(num1)
+        if tokens[0] == "cube":
+            print arithmetic.cube(num1)
 
-    if tokens[0] == "power":
-        print arithmetic.power(num1, num2)
+        if tokens[0] == "power":
+            print arithmetic.power(num1, num2)
 
-    if tokens[0] == "mod":
-        print arithmetic.power(num1, num2)
+        if tokens[0] == "mod":
+            print arithmetic.power(num1, num2)
 
 
 
