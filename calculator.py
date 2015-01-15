d7c11cadb1fb104ae0dfe4e113b59a22688a968b
Hackbright Student <info@hackbrightacademy.com>
2015-01-15 12:21:52 -0800

    REPL and floating point numbers accounted for

--- calculator.py
+++ calculator.py
@@ -14,32 +14,49 @@
 #     otherwise decide which math function to call based on the tokens we read
 import arithmetic
 
-input = raw_input(">")
-tokens = input.split(" ")
-num1 = int(tokens[1])
-if tokens[0] != "cube" and tokens[0] != "square": 
-    num2 = int(tokens[2])
+print """Welcome to our prefix calculator!
+Please enter an operation first, then the number
+or numbers you would like to calculate.
+enter an operator from the following:
++ - * / % square cube power mod
+Enter 'q' to quit calculator
+"""
 
-if tokens[0] == "+":
-    print arithmetic.add(num1, num2)
+while True:
 
-if tokens[0] == "-":
-    print arithmetic.subtract(num1, num2)
+    input = raw_input(">")
+    tokens = input.split(" ")
 
-if tokens[0] == "*":
-    print arithmetic.multiply(num1, num2)
+    if tokens[0] == "q":
+        break
 
-if tokens[0] == "/":
-    print arithmetic.divide(num1, num2)
+    num1 = float(tokens[1])
+    if tokens[0] != "cube" and tokens[0] != "square" and tokens[0] != "q": 
+        num2 = float(tokens[2])
 
-if tokens[0] == "square":
-    print arithmetic.square(num1)
+    if tokens[0] == "+":
+        print arithmetic.add(num1, num2)
 
-if tokens[0] == "cube":
-    print arithmetic.cube(num1)
+    if tokens[0] == "-":
+        print arithmetic.subtract(num1, num2)
 
-if tokens[0] == "power":
-    print arithmetic.power(num1, num2)
+    if tokens[0] == "*":
+        print arithmetic.multiply(num1, num2)
 
-if tokens[0] == "mod":
-    print arithmetic.power(num1, num2)
+    if tokens[0] == "/":
+        print float(arithmetic.divide(num1, num2))
+
+    if tokens[0] == "square":
+        print arithmetic.square(num1)
+
+    if tokens[0] == "cube":
+        print arithmetic.cube(num1)
+
+    if tokens[0] == "power":
+        print arithmetic.power(num1, num2)
+
+    if tokens[0] == "mod":
+        print arithmetic.power(num1, num2)
+
+
+
